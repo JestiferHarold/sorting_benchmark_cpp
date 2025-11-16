c187352dec887be7bec212cf07b00bf791e2cae7
JestiferHarold <sanjay.pandian@tutamail.com>
2025-11-16 12:28:51 +0530

    add cmake

--- test.cpp
+++ test.cpp
@@ -1,25 +1,40 @@
 #include <iostream>
 #include <random>
 #include <vector>
-#include "src/sort.cpp"
+#include <unordered_map>
+#include <string>
+#include <chrono>
+#include "src/header/sort.hpp"
 
 using namespace std;
 
+typedef unordered_map<string, unordered_map<int, int>> unnamed;
+
 int main() {
-    int n; 
+
+    int n;
     cin >> n;
 
     vector<int> ua;
     random_device rd;
 
-    // mt19937
-
     for (int i = 0; i < n; i ++) {
-        ua[i] = rd();
+        ua.push_back(rd());
     }
 
-    Sorting sort(ua);
-    sort.bubble_sort();
-    sort.check_sorted();
+    Sorting sd = Sorting(ua);
+    cout << "Unsorted array\n";
+    // sd.print_unsorted();
+    auto start = chrono::high_resolution_clock::now();  
+    sd.bubble_sort();
+    auto stop = chrono::high_resolution_clock::now();
+    cout << "Sorted array\n";
+    // sd.print_sorted();
+    // sd.check_sortedness();
+
+    auto duration = chrono::duration_cast<chrono::minutes>(stop - start);
+
+    cout << "\nTime taken to do bubble sort for " << n << " elements is " << duration.count();
+
     return 0;
 }
