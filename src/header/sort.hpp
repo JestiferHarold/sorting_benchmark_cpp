c009bc87db8121f1cfa80024068ca5a389311b0f
JestiferHarold <sanjay.pandian@tutamail.com>
2025-11-16 09:55:29 +0530

    change dir layout and some other changes

---
+++ src/header/sort.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <vector>
+
+class Sorting {
+    private:
+        std::vector<int> unsorted_array;
+        std::vector<int> sorted_array;
+
+    public:
+        Sorting(std::vector<int> &vec);
+        void check_sortedness();
+        void print_unsorted();
+        void print_sorted();
+        void bubble_sort();
+        void selection_sort();
+        void insertion_sort();
+        void merge_sort();
+        void quick_sort();
+};
---
+++ src/impls/sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include "../header/sort.hpp"
+
+using namespace std;
+
+Sorting::Sorting(vector<int> &vec) {
+    this->unsorted_array = vec;
+}
+
+void Sorting::check_sortedness() {
+    if (unsorted_array.size() != sorted_array.size()) {
+        cout << "Sorted array has missing elements" << "\n";
+        return;
+    }
+
+    if (sorted_array.size() == 0) {
+        cout << "Array is sorted" << "\n";
+        return;
+    }
+
+    int min = 0;
+
+    for (int i = 1; i < sorted_array.size() - 1; i ++, min = i - 1) {
+        if (sorted_array[min] > sorted_array[i]) {
+            cout << "Array is not sorted properly" << "\n";
+            return;
+        }
+    }
+
+    cout << "Array is sorted well" << "\n";
+}
+
+void Sorting::print_sorted() {
+    for (auto a: sorted_array) {
+        cout << a << "\t";
+    }
+}
+
+void Sorting::print_unsorted() {
+    for (auto a: unsorted_array) {
+        cout << a << "\t";
+    }
+}
